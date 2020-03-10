7b995b4b29aad191ec472f7dd74c2d4342ccf9df
dell <dell@192.168.1.73>
2020-03-10 17:14:04 +0530

    My First Commit

---
+++ src/main/java/MavenProject_Sample/MavenProject_Sample/App.java
@@ -0,0 +1,13 @@
+package MavenProject_Sample.MavenProject_Sample;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/MavenProject_Sample/MavenProject_Sample/Login_Application.java
@@ -0,0 +1,15 @@
+package MavenProject_Sample.MavenProject_Sample;
+
+import org.testng.annotations.Test;
+
+public class Login_Application {
+	
+	
+	@Test
+	
+	public void login()
+	{
+		System.out.println("browser  is launching");
+	}
+
+}
